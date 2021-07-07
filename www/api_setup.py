df02259c6fb9dc8112f0499c18ec74b02c7df727
Deryk DeGuzman <deryk.deguzman@gmail.com>
2021-07-07 15:27:22 -0700

    Add support for specific player/season/stat data retrieval

--- www/api_setup.py
+++ www/api_setup.py
@@ -1,8 +1,9 @@
 from www.features.health_check.resources import HealthCheck
-from www.features.leaders.resources import LeagueLeadersRegular
+from www.features.leaders.resources import LeagueLeadersRegular, LeagueLeaderComparisonRegular
 from www.features.stats.resources import PlayerStatsRegular
 from www.global_dependencies import global_dependencies
 
 global_dependencies.api.add_resource(HealthCheck, '/health-check')
 global_dependencies.api.add_resource(LeagueLeadersRegular, '/leaders/regular')
+global_dependencies.api.add_resource(LeagueLeaderComparisonRegular, '/leaders/regular/compare/<player_key>')
 global_dependencies.api.add_resource(PlayerStatsRegular, '/stats/regular/player/<player_key>')
--- www/features/leaders/resources.py
+++ www/features/leaders/resources.py
@@ -1,6 +1,6 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask_restful import Resource
-from hfdb.queries import get_regular_season_leaders, get_player_data
+from hfdb.queries import get_regular_season_leaders, get_player_data, get_regular_season_stat_by_player_key_and_category
 from . serializers import compile_leaders_and_player_data
 
 
@@ -19,4 +19,22 @@ class LeagueLeadersRegular(Resource):
     return jsonify({
       'leaderboard_type': 'regular',
       'leaders': serialized
+    })
+
+class LeagueLeaderComparisonRegular(Resource):
+  def get(self, player_key):
+    season = request.args.get('season')
+    category = request.args.get('category')
+
+    comparison_stat_model = get_regular_season_stat_by_player_key_and_category(player_key, category, season)[0]
+    comparison_stat_value = getattr(comparison_stat_model, category)
+
+    player_data = get_player_data([player_key])[0]
+
+    return jsonify({
+      'category': category,
+      'player': {
+        'player': player_data.serialize(),
+        'stat': comparison_stat_value
+      }
     })
