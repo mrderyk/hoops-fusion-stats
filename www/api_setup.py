2dc2be757ebd4a5ad431fcc22c3913cc2444610b
Deryk DeGuzman <deryk.deguzman@gmail.com>
2021-07-19 14:02:45 -0700

    add playoff leaders support

--- www/api_setup.py
+++ www/api_setup.py
@@ -1,9 +1,19 @@
+from hfdb.models.player_stats import PlayerStatsPlayoff
 from www.features.health_check.resources import HealthCheck
-from www.features.leaders.resources import LeagueLeadersRegular, LeagueLeaderComparisonRegular
-from www.features.stats.resources import PlayerStatsRegular
+from www.features.leaders.resources import \
+  LeagueLeadersRegular, \
+  LeagueLeaderComparisonRegular, \
+  LeagueLeadersPlayoff, \
+  LeagueLeaderComparisonPlayoff
+from www.features.stats.resources import PlayerStatsRegular, PlayerStatsPlayoff
 from www.global_dependencies import global_dependencies
 
 global_dependencies.api.add_resource(HealthCheck, '/health-check')
+
 global_dependencies.api.add_resource(LeagueLeadersRegular, '/leaders/regular')
 global_dependencies.api.add_resource(LeagueLeaderComparisonRegular, '/leaders/regular/compare/<player_key>')
 global_dependencies.api.add_resource(PlayerStatsRegular, '/stats/regular/player/<player_key>')
+
+global_dependencies.api.add_resource(LeagueLeadersPlayoff, '/leaders/playoff')
+global_dependencies.api.add_resource(LeagueLeaderComparisonPlayoff, '/leaders/playoff/compare/<player_key>')
+global_dependencies.api.add_resource(PlayerStatsPlayoff, '/stats/playoff/player/<player_key>')
--- www/features/leaders/resources.py
+++ www/features/leaders/resources.py
@@ -1,6 +1,11 @@
 from flask import jsonify, request
 from flask_restful import Resource
-from hfdb.queries import get_regular_season_leaders, get_player_data, get_regular_season_stat_by_player_key_and_category
+from hfdb.queries import \
+  get_regular_season_leaders, \
+  get_player_data, \
+  get_regular_season_stat_by_player_key_and_category, \
+  get_playoff_leaders, \
+  get_playoff_stat_by_player_key_and_category
 from . serializers import compile_leaders_and_player_data
 
 
@@ -21,12 +26,54 @@ class LeagueLeadersRegular(Resource):
       'leaders': serialized
     })
 
+
 class LeagueLeaderComparisonRegular(Resource):
   def get(self, player_key):
     season = request.args.get('season')
     category = request.args.get('category')
 
     comparison_stat_models = get_regular_season_stat_by_player_key_and_category(player_key, category, season)
+    if len(comparison_stat_models) == 0:
+      comparison_stat_value = 0
+    else:
+      comparison_stat_model = comparison_stat_models[0]
+      comparison_stat_value = getattr(comparison_stat_model, category)
+
+    player_data = get_player_data([player_key])[0]
+
+    return jsonify({
+      'category': category,
+      'player': {
+        'player': player_data.serialize(),
+        'stat': comparison_stat_value
+      }
+    })
+
+
+class LeagueLeadersPlayoff(Resource):
+  def get(self, date=None):
+    leader_data = get_playoff_leaders(date)
+    player_keys = []
+
+    for entry in leader_data:
+      player_keys_and_stats = entry.player_keys_and_stats
+      player_keys += [pks['player_key'] for pks in player_keys_and_stats]
+
+    player_data = get_player_data(list(set(player_keys)))
+    serialized = compile_leaders_and_player_data(leader_data, player_data)
+
+    return jsonify({
+      'leaderboard_type': 'playoff',
+      'leaders': serialized
+    })
+
+
+class LeagueLeaderComparisonPlayoff(Resource):
+  def get(self, player_key):
+    season = request.args.get('season')
+    category = request.args.get('category')
+
+    comparison_stat_models = get_playoff_stat_by_player_key_and_category(player_key, category, season)
     if len(comparison_stat_models) == 0:
       comparison_stat_value = 0
     else:
